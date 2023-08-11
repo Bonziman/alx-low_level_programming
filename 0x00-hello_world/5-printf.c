e60dd1327149aee2fd3babbe249f9d6e8f3cf242
Bonziman <eloirdiwi@gmail.com>
2023-08-10 17:50:24 -0700

    i love C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Enrty point of the program
+ *
+ * Description: this function prints a text using printf function
+ *
+ * Return: always 0 which is success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
