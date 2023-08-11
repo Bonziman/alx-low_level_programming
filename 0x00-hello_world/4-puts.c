07485da9ac616be5afa9e7df20df00136d045136
Bonziman <eloirdiwi@gmail.com>
2023-08-10 17:19:05 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+	char The_text[] = "\"Programming is like building a multilingual puzzle"; //this sets the var that contains the text
+	puts(The_text); // this is used to print whatever in  the variable
+	return (0);
+
+{
