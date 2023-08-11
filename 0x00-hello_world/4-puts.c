f7cbedc7de363cdbc186e1dbe4f0e3756fa1c665
Bonziman <eloirdiwi@gmail.com>
2023-08-10 17:22:28 -0700

    i am starting to love c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-	char The_text[] = "\"Programming is like building a multilingual puzzle"; //this sets the var that contains the text
-	puts(The_text); // this is used to print whatever in  the variable
-	return (0);
+	char The_text[] = "\"Programming is like building a multilingual puzzle";
+	puts(The_text);
+	return 0;
 
 {
