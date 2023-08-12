5776998d4429dbd279a3dd37b1906a454d589af4
Bonziman <eloirdiwi@gmail.com>
2023-08-11 17:09:03 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the programs entry point
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	char num, lett;
+
+	for (num = 0; num <= 10; num++)
+		putchar(num+'0');
+
+	for (lett = 'a'; lett <= 'g'; lett++)
+		putchar(lett);
+
+	putchar('\n');
+	return (0);
+}
