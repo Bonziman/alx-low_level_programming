91ae62c1d2dc356716e97a97fcd7ba8a9af235cc
Bonziman <eloirdiwi@gmail.com>
2023-08-11 17:14:01 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	char num, lett;
 
-	for (num = 0; num <= 10; num++)
-		putchar(num+'0');
+	for (num = 0; num <= 9; num++)
+		putchar(num + '0');
 
-	for (lett = 'a'; lett <= 'g'; lett++)
+	for (lett = 'a'; lett <= 'f'; lett++)
 		putchar(lett);
 
 	putchar('\n');
