a4d2976f7d09305211028fde6e2ad1f85cdc3706
Bonziman <eloirdiwi@gmail.com>
2023-08-15 12:42:52 -0700

    love alphabets x10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets followed by a newline 10 times
+ *
+ * Return: void
+ */
+void  print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
