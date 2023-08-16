da2e44f75819fc8ef9762e0757e14bf0a88cd866
Bonziman <eloirdiwi@gmail.com>
2023-08-16 03:53:34 -0700

    hey

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,13 +5,14 @@
  * _abs - Function that prints the absulute value of a number
  *
  * Return: always 0
- * 
+ *
  * @c: the variable which contains the absulute value of 'n'
  *
  */
 int _abs(int c)
 {
 	int abs_c;
+
 	abs_c = -1 * c;
 	return (abs_c);
 }
