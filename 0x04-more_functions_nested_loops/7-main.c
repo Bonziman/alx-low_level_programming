895869615948443d48e3e9350c3e98a9c2d33c65
Bonziman <eloirdiwi@gmail.com>
2023-08-18 18:52:10 -0700

    gg

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -10,6 +10,8 @@ int main(void)
     print_diagonal(0);
     print_diagonal(2);
     print_diagonal(10);
-    print_diagonal(-4);
+    print_diagonal(1);
+    print_diagonal(9);
+    print_diagonal(16);
     return (0);
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_diagonal - a function that draws a diagonal line 
+ * print_diagonal - a function that draws a diagonal line
  *
  * @n: number of times the line should be drawen (\)
+ *
+ * @i: just here to help
  *
  * Return: void
  */
 void print_diagonal(int n)
 {
+	int i;
+
 	if (n > 0)
 	{
-		int i;
-
 		for (i = 0; i < n; i++)
 		{
 			_putchar('\\');
