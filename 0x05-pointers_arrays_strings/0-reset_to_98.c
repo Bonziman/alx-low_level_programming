9aba89ed3819f81e47e77e0cbe05853de8038180
Bonziman <eloirdiwi@gmail.com>
2023-08-21 18:10:05 +0100

    to 90s

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function to update the value of a var pointed to by a pointer
  *
- * @c: tht parameter to be changed
+ * @n: tht parameter to be changed
  */
 void reset_to_98(int *n)
 {
