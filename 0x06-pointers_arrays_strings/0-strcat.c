a19de794372e11454936f0241e3918667c929052
Bonziman <eloirdiwi@gmail.com>
2023-08-23 16:53:55 +0100

    concatenating two strings should be easy from now on

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,27 @@
 #include "main.h"
-
+/**
+ * _strcat - a function to concatenate two strings
+ *
+ * @dest: pointer to the destination variable
+ *
+ * @src: pointer to the source string
+ *
+ * Return: pointer to the destination variable
+ *
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *rslt = dest;
 
 	while (*dest != '\0')
 	{
-		*dest++;
+		dest++;
 	}
 	while (*src != '\0')
 	{
 		*dest = *src;
 		dest++;
-		src++
+		src++;
 	}
 	*dest = '\0';
 	return (rslt);
--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -8,13 +8,17 @@
  *     */
 int main(void)
 {
-	char s1[98] = "Bonjour ";
-	char s2[] = "biatch43\n";
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
 	char *ptr;
 
 	printf("%s\n", s1);
 	printf("%s", s2);
-	ptr = _strcat(s1, s2);
+	ptr = _strncat(s1, s2, 1);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	printf("%s\n", ptr);
+	ptr = _strncat(s1, s2, 1024);
 	printf("%s", s1);
 	printf("%s", s2);
 	printf("%s", ptr);
