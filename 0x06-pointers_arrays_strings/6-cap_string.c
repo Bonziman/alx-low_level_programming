288dffb96200adb27257c65ac11bf0b7e080b693
Bonziman <eloirdiwi@gmail.com>
2023-08-24 02:03:57 +0100

    Capitalizing Words Of The World

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * cap_string - a function to capitalize words in a character string seperated 
+ * by spesific special characters.
+ *
+ * @str: pointer to the target string
+ *
+ * Return: pointer to the target string
+ *
+ */
 
 char *cap_string(char *str)
 {
@@ -16,7 +25,7 @@ char *cap_string(char *str)
 		*(str - 1) == 40 || *(str - 1) == 41 || *(str - 1) == 123 ||
 		*(str - 1) == 125)
 		{
-			if (*str != '\0' && *str >=97 && *str <= 122)
+			if (*str != '\0' && *str >= 97 && *str <= 122)
 				*str -= 32;
 		}
 		str++;
