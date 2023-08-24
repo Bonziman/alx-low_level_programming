8f65a9bff0554649288e9524232951fb2d90b0f0
Bonziman <eloirdiwi@gmail.com>
2023-08-24 02:00:34 +0100

    Capitalizing The World

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+char *cap_string(char *str)
+{
+	char *rslt = str;
+
+	if (*str >= 97 && *str <= 122)
+	{
+		*str -= 32;
+	}
+	while (*str != '\0')
+	{
+		if (*(str - 1) == 32 || *(str - 1) == 9 || *(str - 1) == 10 ||
+		*(str - 1) == 44 || *(str - 1) == 59 || *(str - 1) == 46 || 
+		*(str - 1) == 33 || *(str - 1) == 63 || *(str - 1) == 34 ||
+		*(str - 1) == 40 || *(str - 1) == 41 || *(str - 1) == 123 ||
+		*(str - 1) == 125)
+		{
+			if (*str != '\0' && *str >=97 && *str <= 122)
+				*str -= 32;
+		}
+		str++;
+	}
+	return (rslt);
+}
