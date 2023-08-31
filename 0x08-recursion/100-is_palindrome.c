1ac53ef2ff1689147be7190a05f6b2ca80c98a37
Bonziman <eloirdiwi@gmail.com>
2023-08-31 03:49:37 +0100

    palindrome baby

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * str_len - function that counts the length of a string
+ * @s: the string
+ * Return: the length of the rtring
+ */
 int str_len(const char *s)
 {
 	int i = 0;
@@ -10,6 +15,12 @@ int str_len(const char *s)
 	}
 	return (0);
 }
+/**
+ * ispldrm - function to check if string is palindrome or not
+ * @i: a number
+ * @s: string
+ * Return: many values
+ */
 int ispldrm(int i, char *s)
 {
 	if (*s)
@@ -22,6 +33,11 @@ int ispldrm(int i, char *s)
 	}
 	return (1);
 }
+/**
+ * is_palindrome - function to check if a string is a palindrome
+ * @s: the string
+ * Return: 1 if it is, 0 if it's not palindrome
+ */
 int is_palindrome(char *s)
 {
 	int i = 1;
