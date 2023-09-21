62e3188911ab6c1885eebfccb7e34f760f491e22
Bonziman <eloirdiwi@gmail.com>
2023-09-21 16:27:01 +0100

    fixed lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,18 +1,18 @@
-#ifdef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string - (malloc 'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked lists node structure
  */
 typedef struct list_s
 {
@@ -21,4 +21,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-#endif /* LISTS_H*/
+int _putchar(char c);
+unsigned int _strlen(char *s);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LISTS_H */
