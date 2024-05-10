81e788ca1cf32c658187bbbf91216aa8176df901
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-05-10 20:42:15 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
 #include "search_algos.h"
 
 
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
     int i;
     for (i = 0; i <= size; i++)
