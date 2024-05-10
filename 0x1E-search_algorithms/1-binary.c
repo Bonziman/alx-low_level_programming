b05002d5c2e43061416ba1aafb66382506eb8426
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-05-10 20:38:07 +0100

    initial commit + first task

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+
+int binary_search(int *array, size_t size, int value)
+{
+    int i;
+    for (i = 0; i <= size; i++)
+    {
+        if (array[i] == value)
+        {
+            return (i);
+        }
+        else
+        {
+            printf("Value checked array[%d] = [%d]", i, array[i])
+        }
+    }
+
+}
