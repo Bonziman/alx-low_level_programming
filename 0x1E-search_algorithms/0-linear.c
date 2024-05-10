b570aa948b972d2b2e9c69cb71a81eaf566c123a
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-05-10 21:03:16 +0100

    ff

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,12 +5,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+    size_t i;
+    
     if (array == NULL)
     {
         return (-1);
     }
-    size_t i;
     
+
     for (i = 0; i < size; i++)
     {
         if (array[i] == value)
